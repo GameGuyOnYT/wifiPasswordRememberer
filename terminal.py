df5d1e3ca31d294af1e4af19c5a852ae49aca2f9
GameGuy <efeoyun@tahca.com>
2022-10-01 17:47:15 +0300

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -1,14 +1,12 @@
-import subprocess
-from colors import colors
+import subprocess, colors, pyperclip
 
 while True:
-    wifiName = input(colors.OKCYAN + "Enter your wifi name (case doesn't matter): " + colors.ENDC)
-    str1 = subprocess.Popen(f'netsh wlan show profile name="{wifiName}" key=clear',
-                            shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
-    str2 = str1[
-        str1.find("Key Content            : ") + len("Key Content            : "):str1.rfind("Cost settings")].strip()
+    wifiName = input(colors.colors.OKCYAN + "Enter your wifi name (case doesn't matter): " + colors.colors.ENDC)
+    str1 = subprocess.Popen(f'netsh wlan show profile name="{wifiName}" key=clear', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+    str2 = str1[str1.find("Key Content            : ") + len("Key Content            : "):str1.rfind("Cost settings")].strip()
     if not str2.__contains__("tem."):
-        print(colors.OKBLUE + 'Password: ' + colors.OKGREEN + str2 + colors.ENDC)
+        print(colors.colors.OKBLUE + 'Password: ' + colors.colors.OKGREEN + str2 + colors.colors.ENDC)
+        pyperclip.copy(str2)
         quit()
-    elif str2.__contains__("tem."):
-        print(colors.FAIL + "This network is invalid. Maybe a typo?\n" + colors.ENDC)
+    else:
+        print(colors.colors.FAIL + "This network is invalid. Maybe a typo?\n" + colors.colors.ENDC)
